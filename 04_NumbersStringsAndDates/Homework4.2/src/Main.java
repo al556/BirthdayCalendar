a4ebb7a4d82a204b63f0ba30ec12b1934081b820
al556 <al5560791@gmail.com>
2021-03-03 00:44:15 +0200

    Homework 4.2 done

---
+++ 04_NumbersStringsAndDates/Homework4.2/src/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+
+    //byte, short, int  long
+    //float double
+
+    public static void main(String[] args) {
+
+        byte minbyte = Byte.MIN_VALUE;
+        byte maxbyte = Byte.MAX_VALUE;
+
+        short minshort = Short.MIN_VALUE;
+        short maxshort = Short.MAX_VALUE;
+
+        int minint = Integer.MIN_VALUE;
+        int maxint = Integer.MAX_VALUE;
+
+        long minlong = Long.MIN_VALUE;
+        long maxlong = Long.MAX_VALUE;
+
+        float minfloat = Float.MIN_VALUE;
+        float maxfloat = Float.MAX_VALUE;
+
+        double mindouble = Double.MIN_VALUE;
+        double maxdouble = Double.MAX_VALUE;
+        ////////////////////////////////////////
+        System.out.println("byte values Min:"+minbyte+" Max:"+maxbyte);
+        System.out.println("short values Min:"+minshort+" Max:"+maxshort);
+        System.out.println("int values Min:"+minint+" Max:"+maxint);
+        System.out.println("long values Min:"+minlong+" Max:"+maxlong);
+        System.out.println("float values Min:"+minfloat+" Max:"+maxfloat);
+        System.out.println("double values Min:"+mindouble+" Max:"+maxdouble);
+
+
+    }
+
+}
