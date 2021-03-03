f378678093f3b89985421d142697c9942c6f08f7
al556 <al5560791@gmail.com>
2021-03-03 19:28:40 +0200

    Homework 4.4 part 1 done

---
+++ 04_NumbersStringsAndDates/homework_4.4/AlphabetLetters/src/LetterIterator.java
@@ -0,0 +1,18 @@
+public class LetterIterator {
+    public static void main(String[] args) {
+
+        System.out.println("//---------UPPERCASE-CHARACTERS-----------//");
+        for (int i = 65; i<=90;i++){
+            System.out.println("ASCII code "+i+" correspond to "+ (char)i);
+        }
+
+        System.out.println();
+
+        System.out.println("//---------lowercase-characters-----------//");
+        for (int i = 97; i<=122;i++){
+            System.out.println("ASCII code "+(i)+" correspond to "+ (char)(i));
+        }
+
+    }
+
+}
