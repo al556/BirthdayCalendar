adec62ab36c4686449773f5ec850d7d6d82e6abf
al556 <al5560791@gmail.com>
2021-03-11 01:22:48 +0200

    Homework 4.5 part 2 done

--- 04_NumbersStringsAndDates/homework_4.5/SplitText/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/SplitText/src/main/java/Main.java
@@ -2,11 +2,36 @@ public class Main {
 
   public static void main(String[] args) {
 
+
+
   }
 
   public static String splitTextIntoWords(String text) {
     //TODO реализуйте метод
-    return "";
-  }
+    String temp;
+    String returnString = "";
+
+    //clean temp
+    temp = text.replaceAll("[^a-zA-Z\\s\\'\\-\\’]", "");
+    temp = temp.replaceAll("-", " ");
+    temp = temp.replaceAll(" +", " ");
+
+
+    //check string length
+    String[] myStrings = temp.split(" ");
+
+      //creating string
+    for (int i = 0; i < myStrings.length; i++) {
+
+        returnString = returnString + myStrings[i];
+        if(i<myStrings.length-1){
+          returnString = returnString + "\n";
+        }
+
+    }
+
+      return returnString;
+    }
+
 
 }
