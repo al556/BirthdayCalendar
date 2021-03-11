5732f7eeb57b81a7c9a2ea340a09b7c97f808166
al556 <al5560791@gmail.com>
2021-03-11 04:21:52 +0200

    Homework 4.5 extra done

--- 04_NumbersStringsAndDates/homework_4.5/MaskingInformationReqExp/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/MaskingInformationReqExp/src/main/java/Main.java
@@ -1,19 +1,15 @@
 public class Main {
 
-    /*
-    temp = text.replaceAll("[^a-zA-Z\\s\\'\\-\\’]", "");
-    temp = temp.replaceAll("-", " ");
-    temp = temp.replaceAll(" +", " ");
-    String s = "[а-яА-Я-\\s+]+";//\\-?
-*/
-
     public static void main(String[] args) {
 
     }
 
     public static String searchAndReplaceDiamonds(String text, String placeholder) {
+
+        String temp = text.replaceAll("<.*?>",placeholder);
+
         // TODO: реализовать метод, если в строке нет <> - вернуть строку без изменений
-        return text;
+        return temp;
     }
 
 }
