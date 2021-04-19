f25bd1c646089485c320537c0a8a4d2a3f359c70
al556 <al5560791@gmail.com>
2021-04-20 02:53:48 +0300

    Домашняя работа 7.3
    сделано:
    EmailList

--- 07_ArraysAndCollections/homework_7.3/EmailList/src/main/java/EmailList.java
+++ 07_ArraysAndCollections/homework_7.3/EmailList/src/main/java/EmailList.java
@@ -1,15 +1,46 @@
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class EmailList {
 
+    TreeSet<String> set = new TreeSet<>();
+
     public void add(String email) {
         // TODO: валидный формат email добавляется
+
+        String string = email.toLowerCase();
+
+        if(checkIfValid(email)){
+            set.add(string);
+        }else {
+            System.out.println("error");
+        }
     }
 
-    public List<String> getSortedEmails() {
+    public TreeSet<String> getSortedEmails() {
         // TODO: возвращается список электронных адресов в алфавитном порядке
-        return Collections.emptyList();
+
+        List<String> list = new ArrayList<String> (set);
+
+        return set;
     }
+
+    public boolean checkIfValid(String s){
+
+        //String regex = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
+        String regex = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.\\w+$";
+        Pattern pattern = Pattern.compile(regex,Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(s);
+
+        boolean b =matcher.matches();
+
+        return b;
+    }
+
 
 }
+
+
